579f91d618b664d2a694399f84cd303ca6a739a5
sam <sam@silver42.karoo.co.uk>
2017-11-11 23:22:00 +0000

    Few more tween tests

--- unit/tests/TestTween.hx
+++ unit/tests/TestTween.hx
@@ -3,6 +3,33 @@ package tests;
 import macrotween.Tween;
 import utest.Assert;
 
+abstract HelperAbstractFloat(Float) to Float from Float {
+	inline public function new(f:Float) {
+		this = f;
+	}
+}
+
+class HelperObject {
+	public function new() {
+	}
+	public var floatValue:Float = 0;
+	public var nullableFloatValue:Null<Float> = 0;
+	public var integerValue:Int = 0;
+	
+	public var floatProperty(default, set):Float = 0;
+	public function set_floatProperty(f:Float):Float {
+		return this.floatProperty = f;
+	}
+	
+	public var unaryFunction = function(x:Float) {};
+	public var unaryFunctionWithOptionalParam = function(?x:Null<Float>) {};
+	public var unaryFunctionWithDefaultParam = function(x:Float = 0) {};
+	
+	static public var staticFunction = function(x:Float, y:Float) {};
+	static public var staticFunctionWithOptionalParams = function(?x:Null<Float>, ?y:Null<Float>) {};
+	static public var staticFunctionWithDefaultParams = function(x:Float = 0, y:Float = 0) {};
+}
+
 /**
  * Tests the Tween class.
  */
@@ -11,7 +38,7 @@ class TestTween {
 	
 	function linear(x:Float) { return x; }
 	
-	public function testTweenToValue() {
+	public function testTweenSingleFieldToValue() {
 		var myObject:{ x:Float } = { x: 0 };
 		var tween:Tween = Tween.tween(0, 1, myObject.x => 100, linear);
 		
@@ -31,6 +58,17 @@ class TestTween {
 		Assert.isTrue(myObject.x == 100);
 	}
 	
+	public function testTweenSingleFieldWithImplicitStart() {
+		var myObject:{ x:Float } = { x: 50 };
+		var tween:Tween = Tween.tween(0, 1, [myObject.x => _...100], linear);
+		
+		tween.stepTo(0);
+		Assert.isTrue(myObject.x == 50);
+		
+		tween.stepTo(1);
+		Assert.isTrue(myObject.x == 100);
+	}
+	
 	public function testMultiVariableSameValueTween() {
 		var myObject:{ scale:{x:Float, y:Float}, position:{x:Float, y:Float}} = { scale: {x: 0, y: 0}, position: {x: 0, y: 0} };
 		var tween:Tween = Tween.tween(0, 1, myObject => [[scale, position] => [[x, y] => 100]], linear);
@@ -40,31 +78,6 @@ class TestTween {
 
 		tween.stepTo(0.5);
 		Assert.isTrue(myObject.position.x == 50 && myObject.scale.y == 50);
-	}
-	
-	public function testTweenToValue2() {
-		var myObject:{ x:Float } = { x: 50 };
-		var tween:Tween = Tween.tween(0, 1, [myObject.x => 100], linear);
-		
-		tween.stepTo(0);
-		Assert.isTrue(myObject.x == 50);
-		
-		tween.stepTo(0.5);
-		Assert.isTrue(myObject.x == 75);
-		
-		tween.stepTo(1);
-		Assert.isTrue(myObject.x == 100);
-	}
-	
-	public function testTweenToValue3() {
-		var myObject:{ x:Float } = { x: 50 };
-		var tween:Tween = Tween.tween(0, 1, [myObject.x => _...100], linear);
-		
-		tween.stepTo(0);
-		Assert.isTrue(myObject.x == 50);
-		
-		tween.stepTo(1);
-		Assert.isTrue(myObject.x == 100);
 	}
 	
 	public function testTweenRanges() {
@@ -133,7 +146,7 @@ class TestTween {
 		var anotherObject:{ x:Float, y:Float, z:Float } = { x: 0, y : 0, z : 0 };
 		
 		var tween:Tween = Tween.tween(0, 1, [[myObject, anotherObject] => [ x => 100, y => 200]], linear);
-		//TODO
+		
 		Assert.isTrue(myObject.x == 0);
 		
 		tween.stepTo(0.5);
@@ -162,20 +175,40 @@ class TestTween {
 		Assert.isTrue(x == 25);
 	}
 	
-	// TODO we have some variable name conflicts here in the values
+	public function testStaticFunctionTween() {
+		var tween:Tween = Tween.tween(0, 1, [HelperObject.staticFunctionWithDefaultParams(10...20, 10)], linear);
+	}
+	
 	public function testPotentialKeywordConflicts() {
 		var time:Float = 0;
 		var tween:Float = 0;
 		var startValue:Float = 0;
 		var endValue:Float = 0;
 		
-		var t1 = Tween.tween(0, 1, [time => 100], linear); // TODO name clash - silent failure
-		var t2 = Tween.tween(0, 1, [tween => 100], linear); // TODO name clash - compile error
-		var t3 = Tween.tween(0, 1, [startValue => 100], linear); // TODO name clash - silent failure
-		var t4 = Tween.tween(0, 1, [endValue => 100], linear); // TODO name clash - silent failure
+		var t1 = Tween.tween(0, 1, [time => 100], linear);
+		var t2 = Tween.tween(0, 1, [tween => 100], linear);
+		var t3 = Tween.tween(0, 1, [startValue => 100], linear);
+		var t4 = Tween.tween(0, 1, [endValue => 100], linear);
 		
 		for (t in [t1, t2, t3, t4]) t.stepTo(0.5);
 		
 		Assert.isTrue(time == 50 && tween == 50 && startValue == 50 && endValue == 50);
+	}
+	
+	public function testTweenAbstract() {
+		var x:HelperAbstractFloat = new HelperAbstractFloat(50.0);
+		
+		var tween:Tween = Tween.tween(0, 1, [x => 100], linear);
+		
+		Assert.isTrue(x == 50);
+		
+		tween.stepTo(0.0);
+		Assert.isTrue(x == 50);
+		
+		tween.stepTo(0.5);
+		Assert.isTrue(x == 75);
+		
+		tween.stepTo(1.0);
+		Assert.isTrue(x == 100);
 	}
 }
